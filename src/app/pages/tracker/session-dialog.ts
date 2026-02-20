fd65ed8fd9d8ba5857483d9290296796449e29be
Attila Benyi <azurelexis@gmail.com>
2026-02-20 11:03:32 +0100

    Added syncing through websocket

---
+++ src/app/pages/tracker/session-dialog.ts
@@ -0,0 +1,63 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogModule, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { v4 as uuidv4 } from 'uuid';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+
+export interface SessionDialogData {
+  title?: string;
+  message?: string;
+  confirmText?: string;
+  cancelText?: string;
+  icon?: string;
+}
+
+@Component({
+  standalone: true,
+  selector: 'app-session-dialog',
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    FormsModule
+  ],
+  template: `
+    <h2 mat-dialog-title>Connect to session?</h2>
+    <div mat-dialog-content>
+      <mat-form-field>
+          <mat-label>Enter session id</mat-label>
+          <input matInput type="text" [(ngModel)]="sessionId">
+      </mat-form-field>
+    </div>
+    <div mat-dialog-actions align="end">
+      <button mat-button (click)="onCancelClick()">
+        {{ data.cancelText || 'Cancel' }}
+      </button>
+      <button mat-raised-button color="warn" (click)="onConfirmClick()">
+        <mat-icon class="mr-1">connect_without_contact</mat-icon>
+        Connect
+      </button>
+    </div>
+  `,
+})
+export class SessionDialog {
+
+  public sessionId = uuidv4();
+
+  constructor(
+    public dialogRef: MatDialogRef<SessionDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: SessionDialogData,
+  ) {}
+
+  public onCancelClick(): void {
+    this.dialogRef.close(false);
+  }
+
+  public onConfirmClick(): void {
+    this.dialogRef.close(this.sessionId);
+  }
+}
+
--- src/app/pages/tracker/tracker.ts
+++ src/app/pages/tracker/tracker.ts
@@ -13,6 +13,9 @@ import { MatButtonModule, } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { ConfirmationDialog } from './confirmation-dialog';
+import { SessionDialog } from './session-dialog';
+import { webSocket } from 'rxjs/webSocket';
+
 
 
 
@@ -36,6 +39,23 @@ import { ConfirmationDialog } from './confirmation-dialog';
 })
 export class Tracker {
 
+  private websocketMessageTypeSetHost = 'setHost';
+  private websocketMessageTypeSyncRequest = 'syncRequest';
+  private websocketMessageTypeStartSession = 'startSession';
+  private websocketMessageTypeLoadSession = 'loadSession';
+  private websocketMessageTypeResetSession = 'resetSession';
+  private websocketMessageTypeAddPlayer = 'addPlayer';
+  private websocketMessageTypeRemovePlayer = 'removePlayer';
+  private websocketMessageTypeRenamePlayer = 'renamePlayer';
+  private websocketMessageTypeChangeRegion = 'changeRegion';
+  private websocketMessageTypeCatchPokemon = 'catchPokemon';
+  private websocketMessageTypechangePokemonStatus = 'changePokemonStatus';
+  private websocketMessageTypePong = 'pong';
+
+  private websocketClient : any = new WebSocket('https://soul-link-tracker-websocket.onrender.com');
+  public websocketConnected = false;
+  private websocketSessionHost = false;
+
   public playerName : string = '';
   public locationList : Array<LocationStatus> = [];
   public readonly pokemonList : Array<Pokemon> = [];
@@ -58,6 +78,29 @@ export class Tracker {
       this.playerList = this.playerList;
   }
 
+  private connectToWesbocket(uuid : string) {
+    this.sendStartSessionMessage(uuid);
+    this.websocketClient.onopen = () => {
+      console.log('Websocket connected');
+      this.websocketConnected = true;
+    }
+    this.websocketClient.onmessage = (event : MessageEvent<string>) => {
+      this.websocketConnected = true;
+      this.receiveWebsocketMessages(event.data);
+    }
+    this.websocketClient.onerror = () => {
+      console.log('Websocket closed with error');
+      this.websocketSessionHost = false;
+      this.websocketConnected = false;
+    }
+    this.websocketClient.onclose = () => {
+      console.log('Websocket closed');
+      this.websocketSessionHost = false;
+      this.websocketConnected = false;
+    }
+  }
+  
+
   public updateRegionList(region : RegionOption | null) {
     if(region === null){
       this.locationList = [];
@@ -70,6 +113,7 @@ export class Tracker {
           'active': false
         } as LocationStatus
       });
+      this.sendChangeRegionMessage(region.id);
     }
     
   }
@@ -142,8 +186,10 @@ export class Tracker {
     if (this.playerName === '') {
       return;
     }
-    this.playerList.push({ id: this.getNextFreePlayerId(), name: this.playerName, pokemons: [] });
+    const playerDetails = { id: this.getNextFreePlayerId(), name: this.playerName, pokemons: [] };
+    this.playerList.push(playerDetails);
     this.playerName = '';
+    this.sendAddPlayerMessage(playerDetails.id, playerDetails.name);
   }
 
   public removePlayer(player : Player) : void {
@@ -184,6 +230,7 @@ export class Tracker {
     }else{
       existingPokemon.pokemonId = newPokemon.id;
     }
+    this.sendCatchPokemonMessage(player.id, newPokemon.id, location.id);
   }
 
   public onPokemonStatusSelected(location : Location, player : Player, newStatus : 'none' |'missed' | 'caught' | 'fainted') {
@@ -195,7 +242,7 @@ export class Tracker {
       }else{
         playerPokemon.status = newStatus;
       }
-      
+      this.sendChangePokemonStatusMessage(player.id, location.id, newStatus);
     }
   }
 
@@ -247,6 +294,20 @@ export class Tracker {
     dialogRef.afterClosed().subscribe(result => {
       if (result === true) {
         this.removePlayer(player);
+        this.sendRemovePlayerMessage(player.id);
+      }
+    });
+  }
+
+  public openConnectSessionDialog() {
+    const dialogRef = this.dialog.open(SessionDialog, {
+      data: {
+      },
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if(result !== false){
+        this.connectToWesbocket(result);
       }
     });
   }
@@ -267,6 +328,7 @@ export class Tracker {
         this.pokemonControls = {};
         this.playerList = [];
         this.cdr.detectChanges();
+        this.sendResetSessionMessage();
       }
     });
   }
@@ -304,41 +366,9 @@ export class Tracker {
         try {
           const text = reader.result as string;
           const trackSession = JSON.parse(text) as TrackSession;
-
-          this.pokemonControls = {};
-          this.playerList = trackSession.playerList;
-          const region = this.regionList.find(region => region.id === trackSession.regionId) || null;
-          this.regionFormControl.setValue(region);
-
-          if(region !== null){
-            this.locationList = this.locationListProvider.getLocationList(region.id).map( location => {
-              return {
-                'id': location.id,
-                'name': location.name,
-                'status': 'caught',
-                'active': false
-              } as LocationStatus
-            });
-          }
-
-          this.locationList.forEach(location => {
-            this.playerList.forEach(player => {
-              const key = this.getCellKey(location, player);
-              const control = new FormControl<string | Pokemon | null>('');
-
-              const pokemonStatus = player.pokemons.find(p => p.locationId === location.id);
-              if (pokemonStatus) {
-                const pokemon = this.pokemonList.find(p => p.id === pokemonStatus.pokemonId);
-                if (pokemon) {
-                  control.setValue(pokemon);
-                }
-              }
-
-              this.pokemonControls[key] = control;
-            });
-          });
-          console.log(this.locationList);
-          this.cdr.detectChanges();
+          this.restoreSession(trackSession);
+          this.sendLoadSessionMessage();
+          
         } catch (e) {
           console.error('Failed to load session', e);
         }
@@ -350,6 +380,42 @@ export class Tracker {
     input.click();
   }
 
+  public restoreSession(trackSession : TrackSession) {
+    this.pokemonControls = {};
+    this.playerList = trackSession.playerList;
+    const region = this.regionList.find(region => region.id === trackSession.regionId) || null;
+    this.regionFormControl.setValue(region);
+
+    if(region !== null){
+      this.locationList = this.locationListProvider.getLocationList(region.id).map( location => {
+        return {
+          'id': location.id,
+          'name': location.name,
+          'status': 'caught',
+          'active': false
+        } as LocationStatus
+      });
+    }
+
+    this.locationList.forEach(location => {
+      this.playerList.forEach(player => {
+        const key = this.getCellKey(location, player);
+        const control = new FormControl<string | Pokemon | null>('');
+
+        const pokemonStatus = player.pokemons.find(p => p.locationId === location.id);
+        if (pokemonStatus) {
+          const pokemon = this.pokemonList.find(p => p.id === pokemonStatus.pokemonId);
+          if (pokemon) {
+            control.setValue(pokemon);
+          }
+        }
+
+        this.pokemonControls[key] = control;
+      });
+    });
+    this.cdr.detectChanges();
+  }
+
   public getPokemonDetails(id : number) {
     let details = this.pokemonList.find(pokemon => pokemon.id === id)
     if(typeof details === 'undefined'){
@@ -359,7 +425,189 @@ export class Tracker {
     }
     return details;
   }
-  
+
+  public receiveWebsocketMessages(msg : string) {
+    let decodedMessage;
+    if(msg != 'pong'){
+      decodedMessage = JSON.parse(msg);
+    }else{
+      decodedMessage = msg;
+    }
+    
+    const player = this.playerList.find(playerDetails => playerDetails.id === (decodedMessage['playerId'] ?? ''));
+    switch (decodedMessage['type']) {
+      case this.websocketMessageTypePong:
+        this.websocketConnected = true;
+        break;
+      case this.websocketMessageTypeSetHost:
+        this.websocketSessionHost = true;
+        break;
+      case this.websocketMessageTypeSyncRequest:
+        if(this.websocketSessionHost) {
+          this.sendLoadSessionMessage();
+        }
+        break;
+      case this.websocketMessageTypeLoadSession:
+        const trackSession = JSON.parse(decodedMessage['sessionDetails']) as TrackSession;
+        this.restoreSession(trackSession);
+        break;
+      case this.websocketMessageTypeResetSession:
+        this.pokemonControls = {};
+        this.playerList = [];
+        this.cdr.detectChanges();
+        break;
+      case this.websocketMessageTypeAddPlayer:
+        this.playerList.push({ id: decodedMessage['playerId'], name: decodedMessage['playerName'], pokemons: [] });
+        break;
+      case this.websocketMessageTypeRemovePlayer:
+        if(typeof player !== 'undefined'){
+          this.removePlayer(player);
+        }
+        break;
+      case this.websocketMessageTypeRenamePlayer:
+        if(typeof player !== 'undefined'){
+          player.name = decodedMessage['playerName'];
+        }
+        break;
+      case this.websocketMessageTypeChangeRegion:
+        this.locationList = this.locationListProvider.getLocationList(decodedMessage['regionId']).map( location => {
+          return {
+            'id': location.id,
+            'name': location.name,
+            'status': 'caught',
+            'active': false
+          } as LocationStatus
+        });
+        this.regionList.filter(region => region.id === decodedMessage['regionId']).forEach(region => {
+          this.regionFormControl.setValue(region);
+        });
+        break;
+      case this.websocketMessageTypeCatchPokemon:
+        if(typeof player !== 'undefined'){
+          const existingPokemon = player.pokemons.find( pokemonCheck => pokemonCheck.locationId === decodedMessage['locationId']);
+          if(typeof existingPokemon === 'undefined'){
+            player.pokemons.push({
+              'pokemonId': decodedMessage['pokemonId'],
+              'locationId': decodedMessage['locationId'],
+              'status': 'caught'
+            } as PokemonStatus)
+          }else{
+            existingPokemon.pokemonId = decodedMessage['pokemonId'];
+          }
+          this.pokemonList.filter(pokemon => pokemon.id === decodedMessage['pokemonId']).forEach(pokemon => {
+            this.pokemonControls[decodedMessage['locationId'] + '-' + decodedMessage['playerId']].setValue(pokemon);
+          });
+        }
+        break;
+      case this.websocketMessageTypechangePokemonStatus:
+        const existingPokemon = player?.pokemons.find( pokemonCheck => pokemonCheck.locationId === decodedMessage['locationId']);
+        if(typeof existingPokemon !== 'undefined' && typeof player !== 'undefined'){
+          if(decodedMessage['status'] === 'none'){
+            player.pokemons = player.pokemons.filter(pokemon => pokemon.locationId !== decodedMessage['locationId']);
+            this.pokemonControls[decodedMessage['locationId'] + '-' + decodedMessage['playerId']].setValue(null);
+          }else{
+            existingPokemon.status = decodedMessage['status'];
+          }
+        }
+        break;
+      default: 
+        break;
+    }
+    this.cdr.detectChanges();
+  }
+
+  public sendStartSessionMessage(uuid : string) {
+    let msg = {
+      'type': this.websocketMessageTypeStartSession,
+      'uuid': uuid
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendSyncRequest() {
+    let msg = {
+      'type': this.websocketMessageTypeSyncRequest
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendLoadSessionMessage() {
+    let msg = {
+      'type': this.websocketMessageTypeLoadSession,
+      'sessionDetails': JSON.stringify({
+        regionId: this.regionFormControl.value?.id,
+        playerList: this.playerList
+      } as TrackSession)
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendResetSessionMessage() {
+    let msg = {
+      'type': this.websocketMessageTypeResetSession
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendAddPlayerMessage(id: number, name :string) {
+    let msg = {
+      'type': this.websocketMessageTypeAddPlayer,
+      'playerId': id,
+      'playerName': name
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendRenamePlayerMessage(id: number, name :string) {
+    let msg = {
+      'type': this.websocketMessageTypeRenamePlayer,
+      'playerId': id,
+      'playerName': name
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendRemovePlayerMessage(id: number) {
+    let msg = {
+      'type': this.websocketMessageTypeRemovePlayer,
+      'playerId': id
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendChangeRegionMessage(id: number) {
+    let msg = {
+      'type': this.websocketMessageTypeChangeRegion,
+      'regionId': id
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendCatchPokemonMessage(playerId: number, pokemonId :number, locationId: number) {
+    let msg = {
+      'type': this.websocketMessageTypeCatchPokemon,
+      'playerId': playerId,
+      'pokemonId': pokemonId,
+      'locationId': locationId
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+  public sendChangePokemonStatusMessage(playerId: number, locationId: number, status: string) {
+    let msg = {
+      'type': this.websocketMessageTypechangePokemonStatus,
+      'playerId': playerId,
+      'locationId': locationId,
+      'status': status
+    };
+    this.sendWebsocketMessages(msg);
+  }
+
+
+  public sendWebsocketMessages(msg : object) {
+    const codedMessage = JSON.stringify(msg);
+    this.websocketClient.send(codedMessage);
+  }
 }
 
 export interface Player {
